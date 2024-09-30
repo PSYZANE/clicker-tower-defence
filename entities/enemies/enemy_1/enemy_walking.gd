b8b9bd669042e65b3e5050addebfd8f3abe858a9
PSYZANE <91477271+PSYZANE@users.noreply.github.com>
2024-09-30 16:44:07 +0530

    add initial files

---
+++ entities/enemies/enemy_1/enemy_walking.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var target :Vector2
+var direction: Vector2 = Vector2.RIGHT
+@export var speed :int = 200
+@export var damage :int
+@export var health :int = 10
+
+func _ready() -> void:
+	target = Global.player_pos
+
+func _process(_delta: float) -> void:
+	direction = (target-global_position).normalized()
+	position += direction * speed * _delta
---
+++ entities/player/button/button_base.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+var level:int = 1
+
+func _ready() -> void:
+	Global.player_pos = global_position
